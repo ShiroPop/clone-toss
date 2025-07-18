8ce71d49977e99ead94c8b7cb90725ca5c1ef2a0
Hayeon-Jung <jhy000714@gmail.com>
2025-07-18 22:39:35 +0900

    링크, 문구를 따로 관리하여 코드 가독성 개선

--- src/Layout/HomeSection.tsx
+++ src/Layout/HomeSection.tsx
@@ -2,6 +2,17 @@ import "../styles/layout/HomeSection.sass";
 import "../styles/abstracts/Mixins.sass";
 
 const HomeSection = () => {
+  const homeScreen1 = "https://static.toss.im/assets/homepage/newtossim/home_screen_1.png";
+  const homeScreen2 = "https://static.toss.im/assets/homepage/newtossim/home_screen_2.png";
+  const shadow = "https://static.toss.im/assets/homepage/newtossim/iPhone15_Clay_Shadow_03.png";
+
+  const titleText = `내 돈 관리,
+지출부터 일정까지
+똑똑하게`;
+  const subText = `토스에 계좌와 카드를 연결해보세요.
+계좌 잔액, 대출·투자 내역은 기본,
+일자별 소비와 수입까지 한 번에 볼 수 있어요.`;
+
   return (
     <section className="sec">
       <div className="homesection_padding">
@@ -9,35 +20,19 @@ const HomeSection = () => {
           <div className="homesection_height">
             <div className="title_wrap">
               <h1 className="category">홈 · 소비</h1>
-              <h2 className="title">{`내 돈 관리,
-지출부터 일정까지
-똑똑하게`}</h2>
+              <h2 className="title">{titleText}</h2>
             </div>
             <div className="homesection_imgs_wrap">
               <div className="homesection_imgbox_first">
-                <img
-                  className="homesection_img"
-                  src="https://static.toss.im/assets/homepage/newtossim/home_screen_1.png"
-                />
-                <img
-                  className="homesection_img_shadow"
-                  src="https://static.toss.im/assets/homepage/newtossim/iPhone15_Clay_Shadow_03.png"
-                />
+                <img className="homesection_img" src={homeScreen1} />
+                <img className="homesection_img_shadow" src={shadow} />
               </div>
               <div className="homesection_imgbox_second">
-                <img
-                  className="homesection_img"
-                  src="https://static.toss.im/assets/homepage/newtossim/home_screen_2.png"
-                />
-                <img
-                  className="homesection_img_shadow"
-                  src="https://static.toss.im/assets/homepage/newtossim/iPhone15_Clay_Shadow_03.png"
-                />
+                <img className="homesection_img" src={homeScreen2} />
+                <img className="homesection_img_shadow" src={shadow} />
               </div>
             </div>
-            <p className="text homesection_text">{`토스에 계좌와 카드를 연결해보세요.
-계좌 잔액, 대출·투자 내역은 기본,
-일자별 소비와 수입까지 한 번에 볼 수 있어요.`}</p>
+            <p className="text homesection_text">{subText}</p>
           </div>
         </div>
       </div>
